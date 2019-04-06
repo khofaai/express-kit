4dd12f120a31a812d0748f1a34a276ef6d470ff7
Inani El Houssain <inanielhoussain@gmail.com>
2019-04-06 10:10:03 +0100

    Add quick Access to server

--- src/vendor/container.js
+++ src/vendor/container.js
@@ -28,9 +28,10 @@ export default class Container {
 	runServer() {
 		this.mapRoutes();
 		this.httpServer.listen(env.PORT, () => {
-			console.log("####################################")
-			console.log(`#### App running at port : ${env.PORT} ####`)
-			console.log("####################################")
+			console.log("####################################");
+			console.log(`#### App running at port : ${env.PORT} ####`);
+			console.log("####################################");
+			console.log("\x1b[32m", `http://${env.DB_HOSTNAME}:${env.PORT}`);
 		});
 	}
 }
